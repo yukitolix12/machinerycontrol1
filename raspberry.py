dc2cae329e7091cf8bd7b03542efb452566a14f2
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2026-04-05 17:43:47 +0900

    初期設定の定義をする

--- raspberry.py
+++ raspberry.py
@@ -3,3 +3,9 @@ import time
 
 # PIN番号の定義(名前をつける)
 CONTROL_PIN = 18
+
+def setup():
+    """② 初期設定"""
+    GPIO.setmode(GPIO.BCM)        # BCM（役割別）番号で指定
+    GPIO.setup(CONTROL_PIN, GPIO.OUT) # 18番ピンを出力用に設定
+    print("セットアップ完了。制御を開始します...")
