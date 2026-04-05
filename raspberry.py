085dcf4ac85abaca1751c1eba3ae4e58015338eb
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2026-04-05 17:44:35 +0900

    loopのロジックを作成

--- raspberry.py
+++ raspberry.py
@@ -9,3 +9,15 @@ def setup():
     GPIO.setmode(GPIO.BCM)        # BCM（役割別）番号で指定
     GPIO.setup(CONTROL_PIN, GPIO.OUT) # 18番ピンを出力用に設定
     print("セットアップ完了。制御を開始します...")
+
+def main_loop():
+    """③ メインの制御ロジック"""
+    while True:
+        # 例：1秒おきにON/OFFを繰り返す（Lチカなど）
+        GPIO.output(CONTROL_PIN, GPIO.HIGH) # ONにする
+        print("機器を稼働中...")
+        time.sleep(1.0)
+        
+        GPIO.output(CONTROL_PIN, GPIO.LOW)  # OFFにする
+        print("機器を停止中...")
+        time.sleep(1.0)
